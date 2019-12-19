fc6668a745b9dc52aa0d5820dc8354c6f5707549
gal <galseidner@gmail.com>
2019-12-19 12:36:57 +0200

    33

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int main() {
   std::cout << "Hello, World!" << std::endl;
   std::cout << "Hello, World!" << std::endl;
   std::cout << "Hello, World!" << std::endl;
+  std::cout << "Hello, World 2!" << std::endl;
 
 
   return 0;
